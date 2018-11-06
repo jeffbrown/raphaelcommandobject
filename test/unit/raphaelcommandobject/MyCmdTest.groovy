b0044986c7330c8b149827b5fa8cf29d53d00202
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2018-11-06 10:36:03 +0100

    Adds a unit test against `dataBind`.

--- test/unit/raphaelcommandobject/MyCmdTest.groovy
+++ test/unit/raphaelcommandobject/MyCmdTest.groovy
@@ -1,5 +1,7 @@
 package raphaelcommandobject
 
+
+import org.codehaus.groovy.grails.web.binding.DataBindingUtils
 import spock.lang.Specification
 
 class MyCmdTest extends Specification {
@@ -27,5 +29,19 @@ class MyCmdTest extends Specification {
         cmd != null
         cmd.description == "Test"
         cmd.someType == SomeType.Foo
+    }
+
+    void "data bind works"() {
+        given:
+        def values = [description: "Test", someType: "foos"]
+
+        when:
+        def cmd = new MyCmd()
+        DataBindingUtils.bindObjectToInstance(cmd, values)
+
+        then:
+        cmd != null
+        cmd.description == "Test"
+        cmd.someType == SomeType.Foo // note how this fails
     }
 }
